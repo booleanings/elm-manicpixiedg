31647f45d178a7ab876fc5b1c6bddd8bd6a00185
Aida El Kouri <aelkouri@swampfox.com>
2019-01-11 16:17:37 -0500

    trying to add rest endpoint

---
+++ src/Rest.elm
@@ -0,0 +1,20 @@
+module ManicPixie.Rest exposing (fetchNames)
+
+import Http exposing (..)
+import Json.Decode as Decode exposing (Decoder)
+import Types exposing (..)
+
+import Json.Decode as D
+
+type Msg = GotItems (Result Http.Error (List String))
+
+fetchNames : Cmd Msg
+fetchNames =
+  Http.get
+    { url = "http://names.drycodes.com/10?nameOptions=girl_names"
+    , expect = Http.expectJson GotItems (D.list (D.field "name" D.string))
+    }
+-- DECODER
+nameDecoder : Decoder String
+nameDecoder =
+  (Decode.index 0 Decode.string)
